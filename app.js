82cf2258df801356c150ec564add35022f3a5c99
mortaca123 <124174458+mortaca123@users.noreply.github.com>
2023-02-01 16:24:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({origin: '*'}));
 
 app.use('/', indexRouter);
 app.use('/quotes', quotesRouter);
